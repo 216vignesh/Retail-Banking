7615c3f9c5e51768b5031b761e188b2160b724ed
Mihir <chitremihir@gmail.com>
2020-06-12 23:55:33 +0530

    Customer registration added

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app=Flask(__name__)
 @app.route('/')
 def loginpage():
 	return render_template('login.html')
+
+@app.route('/createcustomer',methods=['GET','POST'])
+def createcustpage():
+	return render_template('create_customer.html')
